37f105c16356b90d41314c2f2727bced1617dfce
root <root@LAPTOP-F2PA8QOO.localdomain>
2021-03-06 17:09:32 -0300

    oxygen and hydrogen threads logic

--- WaterProducer/water_producer.c
+++ WaterProducer/water_producer.c
@@ -1,28 +1,79 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
 #include <semaphore.h>
 
+pthread_mutex_t mutex;
+
 sem_t oxygen;
 sem_t hydrogen;
 
-pthread_mutex_t mutex;
+pthread_barrier_t barrier;
 
 int oxygenCounter;
 int hydrogenCounter;
+int molecule;
 
-void *t_function(void *input)
-{
+void *oxygen_thread(void *input) {
     int t_id = *(int *)input;
-    int value;
+
+    pthread_mutex_lock(&mutex);
+    oxygenCounter++;
+    if (hydrogenCounter >= 2) {
+       // printf("[Oxygen] Entrou if para liberar threads \n");
+        sem_post(&hydrogen);
+        sem_post(&hydrogen);
+        hydrogenCounter -= 2;
+        sem_post(&oxygen);
+        oxygenCounter--;
+    } else {
+        pthread_mutex_unlock(&mutex);
+    }
+
+    sem_wait(&oxygen);
+
+    printf("Oxigenio ( %d ) se ligou para formar a molecula \n", t_id);
+
+    pthread_barrier_wait (&barrier);
+
+    printf("Molecula criada \n");
+
+    molecule++;
+
+    pthread_mutex_unlock(&mutex);
 
     return 0;
 }
 
-int main(int argc, char *argv[])
-{
+void *hydrogen_thread(void *input) {
+    int t_id = *(int *)input;
+
+    pthread_mutex_lock(&mutex);
+    hydrogenCounter++;
+    if (hydrogenCounter >= 2 && oxygenCounter >= 1) {
+       // printf("[Hydrogen] Entrou if para liberar threads \n");
+        sem_post(&hydrogen);
+        sem_post(&hydrogen);
+        hydrogenCounter -= 2;
+        sem_post(&oxygen);
+        oxygenCounter--;
+    } else {
+        pthread_mutex_unlock(&mutex);
+    }
+
+    sem_wait(&hydrogen);
+
+    printf("Hidrogenio ( %d ) se ligou para formar a molecula \n", t_id);
+
+    pthread_barrier_wait (&barrier);
+
+    pthread_mutex_unlock(&mutex);
+
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
     int molecules;
     molecules = atoi(argv[1]);
 
@@ -31,39 +82,37 @@ int main(int argc, char *argv[])
     int ids[molecules * 2];
 
     int i;
+    
+    pthread_mutex_init(&mutex, NULL);
 
     sem_init(&oxygen, 0, 0);
     sem_init(&hydrogen, 0, 0);
 
-    pthread_mutex_init(&mutex, NULL);
+    pthread_barrier_init (&barrier, NULL, 3);
 
-    for (i = 0; i < molecules * 2; i++)
-    {
+    for (i = 0; i < molecules * 2; i++) {
         ids[i] = i;
 
         if (i < molecules)
-        {
-            pthread_create(&t_oxygen[i], NULL, t_function, (void *)&ids[i]);
-        }
+            pthread_create(&t_oxygen[i], NULL, oxygen_thread, (void *)&ids[i]);
 
-        pthread_create(&t_hydrogen[i], NULL, t_function, (void *)&ids[i]);
+        pthread_create(&t_hydrogen[i], NULL, hydrogen_thread, (void *)&ids[i]);
     }
 
-    for (i = 0; i < molecules * 2; i++)
-    {
+    for (i = 0; i < molecules * 2; i++) {
         ids[i] = i;
 
         if (i < molecules)
-        {
             pthread_join(t_oxygen[i], NULL);
-        }
 
         pthread_join(t_hydrogen[i], NULL);
     }
 
-    sem_destroy(&t_hydrogen);
-    sem_destroy(&t_oxygen);
+    sem_destroy(&oxygen);
+    sem_destroy(&hydrogen);
     pthread_mutex_destroy(&mutex);
+
+    printf("Foram criadas ( %d ) moleculas \n", molecule);
 
     return 0;
 }
